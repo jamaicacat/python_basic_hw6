64eb0d02ca4a987db1555a228f22c397d38f4b9e
Nikita Klochko <mandicis173@gmail.com>
2022-11-07 18:11:17 +0200

    hw 6: done

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ is_searched_in_c = searched_subset.issubset(set_c)
 res_1 = set()
 res_2 = set()
 
-for item in res:
-    if item % 2 == 0:
-        res_1.add(item)  # attention!: кортеж из задания 1 не содержит значение "0"
+for key in res:
+    if key % 2 == 0:
+        res_1.add(key)  # attention!: кортеж из задания 1 не содержит значение "0"
     else:
-        res_2.add(item)
+        res_2.add(key)
 
 # DICT
 dict_a = {
@@ -63,10 +63,39 @@ dict_c = {
 
 # Використовуючи словники dict_a і dict_b
 # 1. Зробити загальний словник
-dict_a_b = {[*dict_a.items(), *dict_b.items()]}
+dict_a_b = dict([*dict_a.items(), *dict_b.items()])
 # или так, но это я увидел когда гуглил и такую распаковку мы не проходили, кажется
 # dict_a_b = {**dict_a, **dict_b}
 
 # 2. Взяти ключі словника dict_a і значення словника dict_b і зробити загальний словник:
 # res = {'key_1': 'value_6', 'key_2': 'value_7', 'key_3': 'value_8', 'key_4': 'value_9', 'key_5': 'value_10'}
-dict_res = {zip(dict_a.keys(), dict_b.values())}
+dict_res_2 = dict(zip(dict_a.keys(), dict_b.values()))
+
+# 3. Взяти значення словника dict_b і ключі словника dict_a і зробити загальний словник:
+# res = { 'value_6': 'key_1', 'value_7': 'key_2', 'value_8': 'key_3', 'value_9': 'key_4', 'value_10': 'key_5'}
+dict_res_3 = dict(zip(dict_b.values(), dict_a.keys()))
+
+# 4. Використовуючи результат з першого завдання res зробити словник, де будуть тільки непарні числа в закінченнях ключей і значень
+# res = { 'key_1': 'value_1', 'key_3': 'value_3', 'key_5': 'value_5', 'key_7': 'value_7', 'key_9': 'value_9', 'key_11': 'value_11' }
+dict_res_4 = {}
+
+for key, value in dict_a_b.items():
+    key_index = int(key.split('_')[1])
+
+    if key_index % 2 != 0:
+        dict_res_4.update({key: value})
+
+# 5. Створити словник де ключем буде ім'я словника dict_a або dict_b,
+# а значенням буде кількість пар-ключ значення які знаходяться в dict_a або dict_b зі словника dict_c
+# res = { 'dict_a': 2, 'dict_b': 3}
+set_dict_a = set(dict_a)
+set_dict_b = set(dict_b)
+set_dict_c = set(dict_c)
+
+intersection_a_c = set_dict_c.intersection(set_dict_a)
+intersection_b_c = set_dict_c.intersection(set_dict_b)
+
+dict_res_5 = {
+    'dict_a': len(intersection_a_c),
+    'dict_b': len(intersection_b_c)
+}
